6308c1af7ffe5a0dd98a467f9a117d941f6a4363
Petya Krysteva <petya.nikolova98@gmail.com>
2022-02-24 23:37:25 +0200

    Development news (#4)
    * Added News Api services
    
    * New storyboard for News
    
    * Added NewsViewController
    
    * Config News View Controller
    
    Co-authored-by: pnkrusteva <petyakrysteva>

---
+++ LeoNFT/MainStructure/Model/NewsModel.swift
@@ -0,0 +1,16 @@
+//
+//  NewsModel.swift
+//  LeoNFT
+//
+//  Created by Petya Krysteva on 24.02.22.
+//
+
+import Foundation
+import UIKit
+
+struct News: Codable {
+    var title: String?
+    var url: String?
+    var source: String?
+    var img: String?
+}
---
+++ LeoNFT/MainStructure/View/NewsViewCell.swift
@@ -0,0 +1,59 @@
+//
+//  NewsViewCell.swift
+//  LeoNFT
+//
+//  Created by Petya Krysteva on 24.02.22.
+//
+
+import Foundation
+
+import UIKit
+
+class NewsTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var img: UIImageView!
+    
+    @IBOutlet weak var dataTxt: UILabel!
+    var url: String = ""
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+        dataTxt.numberOfLines = 0
+        addShadowAndRoundedCorners()
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    @IBAction func shareNews(_ sender: Any) {
+        let text = dataTxt.text
+        let image = img.image
+        let myWebsite = NSURL(string:url)
+        let shareAll = [text! , image! , myWebsite] as [Any]
+        let activityViewController = UIActivityViewController(activityItems: shareAll, applicationActivities: nil)
+        activityViewController.popoverPresentationController?.sourceView = UIApplication.shared.keyWindow?.rootViewController?.view
+        
+        activityViewController.isModalInPresentation = true
+        UIApplication.shared.keyWindow?.rootViewController?.present(activityViewController, animated: true, completion: nil)
+    }
+    
+    func addShadowAndRoundedCorners() {
+        let radius: CGFloat = 20
+        contentView.layer.cornerRadius = radius
+        contentView.layer.borderWidth = 2
+        contentView.layer.borderColor = UIColor(red: 226.0 / 255.0, green: 232.0 / 255.0, blue: 240.0 / 255.0, alpha: 1.0).cgColor
+        contentView.layer.masksToBounds = true
+        
+        layer.shadowColor = UIColor.black.cgColor
+        layer.shadowOffset = CGSize(width: 0, height: 2.0)
+        layer.shadowRadius = 5.0
+        layer.shadowOpacity = 0.1
+        layer.masksToBounds = false
+        layer.shadowPath = UIBezierPath(roundedRect: bounds, cornerRadius: radius).cgPath
+        layer.cornerRadius = radius
+    }
+
+}
